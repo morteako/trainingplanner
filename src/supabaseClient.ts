66d3960ab03231f94a187dfeedf1399bc7984df5
Morten Kolstad <morten.kolstad@bekk.no>
2026-01-24 21:30:59 +0100

    :

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import { supabaseConfig } from './supabaseConfig'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  supabaseConfig.url,
+  supabaseConfig.anonKey
+)
